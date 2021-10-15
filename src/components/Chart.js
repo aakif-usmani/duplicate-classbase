d069795ed4d6fd9cd8e4e0087942cd1534ff384d
Aakif usmani <aakifusmanismas@gmail.com>
2021-10-15 17:45:12 +0500

    mmm

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -3,8 +3,8 @@ import "./Chart.css";
 
 class Chart extends react.Component {
   constructor(props) {
-    super();
-    this.state = {};
+    super(props);
+    this.state = { filteredYear: "" };
   }
   totalExpense = 0;
   monthCount = [
@@ -21,16 +21,42 @@ class Chart extends react.Component {
     { label: "Nov", value: 0 },
     { label: "Dec", value: 0 },
   ];
+ 
 
   render() {
+    let Expenses =
+    this.state.filteredYear === "all"
+      ? this.props.expense
+      : this.props.expense.filter((filter1) => {
+        if (filter1.date.getFullYear().toString() === this.state.filteredYear ) {
+          let month = filter1.date.getMonth();
+          this.monthCount[month].value += filter1.amount;
+          this.totalExpense += filter1.amount;
+          return true;
+        }else{
+          return false;
+        }
+      });
+   
     return (
+      this.state.filteredYear !== "all" &&(
       <div className="chart-main">
-        <p>
-          <b>Chart</b>
-        </p>
+        <div className="chartDiv-binder">
+        {this.monthCount.map((month, index) =>{ 
+              return (
+                <div className="chart-vertical-div" key={index}>
+                  <div
+                    className="chart-fill"
+                    style={{ height: (month.value / this.totalExpense) * 100 + "%" }}
+                  ></div>
+                  <p className="month-name">{month.label}</p>
+                </div>
+              );
+            })}
+            </div>
       </div>
-    );
+    ));
   }
-}
+};
 
 export default Chart;
